b339e8b39470885c03fa6befef7d9a306aaceaf9
neelyab <neelyab@vcu.edu>
2023-11-15 09:12:50 -0500

    added post info and previous post functionality

--- src/PostCarousel.js
+++ src/PostCarousel.js
@@ -8,6 +8,8 @@ class PostCarousel extends React.Component {
       posts: [],
       url: "",
       title: "",
+      date: "",
+      postLink: "",
       hasError: false
     };
   }
@@ -16,20 +18,31 @@ class PostCarousel extends React.Component {
     if (!response.ok) {
       this.setState({hasError: true})
     }
-
     const posts = await response.json();
     return posts;
   }
-  changePost = () => {
+  nextPost = () => {
     let numPosts = this.state.posts.length - 1;
     if (this.state.count < numPosts) {
       this.setState({ count: (this.state.count += 1) });
       this.updateUrl();
-      this.updateTitle();
+      this.updatePostInfo();
     } else if (this.state.count == numPosts) {
       this.setState({ count: (this.state.count -= numPosts) });
       this.updateUrl();
-      this.updateTitle();
+      this.updatePostInfo();
+    }
+  };
+  previousPost = () => {
+    let numPosts = this.state.posts.length - 1;
+    if (this.state.count > 0) {
+      this.setState({ count: (this.state.count -= 1) });
+      this.updateUrl();
+      this.updatePostInfo();
+    } else if (this.state.count == 0) {
+      this.setState({ count: (this.state.count += numPosts) });
+      this.updateUrl();
+      this.updatePostInfo();
     }
   };
   updateUrl() {
@@ -43,14 +56,20 @@ class PostCarousel extends React.Component {
       this.setState({ url: "null" });
     }
   }
-  updateTitle() {
-    this.setState({ title: this.state.posts[this.state.count].title.rendered });
+  updatePostInfo() {
+    let localeString = new Date(this.state.posts[this.state.count].date).toLocaleDateString();
+    this.setState({ 
+      title: this.state.posts[this.state.count].title.rendered,
+      date: localeString,
+      postLink: this.state.posts[this.state.count].link
+     });
+
   }
   componentDidMount() {
     this.loadPosts().then((posts) => {
       this.setState({ posts: [...posts] });
       this.updateUrl();
-      this.updateTitle();
+      this.updatePostInfo();
     });
   }
 
@@ -63,18 +82,19 @@ class PostCarousel extends React.Component {
         {this.state.posts ? (
           <>
             <div className="recent-post-image">
-              {this.state.url != "null" ? <img src={this.state.url}></img> : ""}
+              {this.state.url != "null" ? <img src={this.state.url} alt={this.state.title}></img> : ""}
+              <i class="fa-solid fa-chevron-left" onClick={() => this.previousPost()}></i>
+              <i class="fa-solid fa-chevron-right" onClick={() => this.nextPost()}></i>
             </div>
             <div className="recent-post-info">
+              <span className="date-banner">{this.state.date}</span>
               <h3>{this.state.title}</h3>
+              <a href={this.state.postLink}><button>READ MORE</button></a>
             </div>
           </>
         ) : (
           ""
         )}
-        <button className="arrow" onClick={() => this.changePost()}>
-          click
-        </button>
       </div>
     );
   }
